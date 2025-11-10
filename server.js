a350b3074081178e409508bdde2c1fa1c64c73a7
Francisco Miranda <jaramira2001@gmail.com>
2025-11-09 19:47:23 -0700

    se agrego la pagina de indice cap cds en la raiz

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const dotenvXConfig = require('./src/config/dotenvXConfig');
 const mongoose = require('./src/config/connectToMongoDB');
 require('./src/api/models/mongodb/security/ztlabels');
 const errorHandler = require('./src/middlewares/errorHandler'); 
+const path = require('path');
 
 module.exports = async (o) => {
   try {
     let app = express();
     app.express = express;
+
+    // Servir la página de índice de CDS en la raíz
+    const cds_launchpad_app = path.dirname(require.resolve('@sap/cds/app/index.html'));
+    app.use('/', express.static(cds_launchpad_app));
 
     // Captura JSON inválido ANTES que llegue a las rutas
     app.use(express.json({ limit: '500kb' }));
